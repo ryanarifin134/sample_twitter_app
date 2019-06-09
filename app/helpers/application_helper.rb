f9a5913f3548cbc6b2b16bfa18599397685a46d6
Ryan Arifin <ryanarifin134@gmail.com>
2019-06-09 15:00:58 -0700

    Added a full_title view helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Return full title on a per-page basis
+  def full_title(page_title = "")
+    result_title = "Ruby on Rails Tutorial Sample App"
+
+    if !page_title.empty?
+        result_title = page_title + " | " + result_title
+    end
+    result_title
+  end
+
 end
